52e696e2d39e7b48a18aa7c3da24897262d790bb
wiktorwieclaw <lonevixu@gmail.com>
2021-04-02 02:42:19 +0200

    added rooms to server and joining procedure to front

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,58 @@
 import './App.css';
 import {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Switch, useHistory, useParams} from 'react-router-dom';
 import io from 'socket.io-client';
 
 const socket = io('localhost:3001');
 
-function App() {
+function RoomSelection() {
+    const history = useHistory();
+
+    useEffect(() => {
+        socket.on('joined room', (id) => {
+            history.push(`/room/${id}`);
+        });
+
+        return () => {
+            socket.off('joined room');
+        };
+    });
+
+    const handleButton = id => {
+        socket.emit('join room', id);
+    };
+
+    return (
+        <div>
+            <button onClick={() => handleButton(1)}>room1</button>
+            <button onClick={() => handleButton(2)}>room2</button>
+        </div>
+    );
+}
+
+function Room() {
+    let {id} = useParams();
+
+    if (!(['1', '2'].includes(id))) {
+        throw "wrong room id";
+    }
+
     const [inputValue, setInputValue] = useState('');
     const [messages, setMessages] = useState([]);
 
     useEffect(() => {
-        socket.on('chat message', (msg) => {
+        socket.on('chat message', msg => {
             setMessages([...messages, msg]);
         });
 
         return () => {
             socket.off('chat message');
         };
-    })
+    });
 
     const handleChange = event => {
         setInputValue(`${event.target.value}`);
-    }
+    };
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -43,6 +75,17 @@ function App() {
             </form>
         </div>
     );
+}
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" component={RoomSelection} exact/>
+                <Route path="/room/:id" component={Room}/>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App;
